fe06871263c0892fd5c100d1ff3472cfdfccf943
Aisa Nicevic <aisanicevic04@gmail.com>
2022-05-28 11:49:31 +0200

    py

---
+++ pocetak.py
@@ -0,0 +1,33 @@
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964, "year": 2002}
+# # print(thisdict["year"])
+# print(len(thisdict))
+
+
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# x = thisdict["model"]
+# print(x)
+
+
+# car = {"brand": "Ford", "model": "Mustang", "year": 1964}
+
+# x = car.keys()
+
+# print(x)  # before the change
+
+# car["color"] = "white"
+
+# print(x)  # after the change
+
+# car = {"brand": "Ford", "model": "Mustang", "year": 1964}
+
+# x = car.values()
+
+# print(x)  # before the change
+
+# car["year"] = 2020
+
+# print(x)  # after the change
+
+
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+thisdict.update({"year": 2020})
