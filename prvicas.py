6956f0664355bd7641b0b54bfc9d52f5783ffb44
Aisa Nicevic <aisanicevic04@gmail.com>
2022-06-21 20:42:50 +0200

    class

---
+++ prvicas.py
@@ -0,0 +1,15 @@
+# a = int(input("unesite prvi broj:  "))
+# b = int(input("unesite drugi broj: "))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# ime=str(input("unesite svoje ime:"))
+# prezime=str(input("unesite svoje prezime:"))
+# print(ime+prezime)
+
+a=int(input("unesite neki broj"))
+if a>10:
+    print("broj je veci od 10")
+else:
+    print("broj je manji od 10")
